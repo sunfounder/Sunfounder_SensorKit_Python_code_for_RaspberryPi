e79609c2c5c90c370aaefcce82f6f063144fa90a
Sunfounder <chuanmorison@gmail.com>
2015-04-01 03:50:41 +0000

    add a new file

---
+++ 25_metalTouch.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+import RPi.GPIO as GPIO
+
+TouchPin = 11
+LedPin   = 12
+
+def setup():
+	GPIO.setmode(GPIO.BOARD)       # Numbers GPIOs by physical location
+	GPIO.setup(LedPin, GPIO.OUT)   # Set LedPin's mode is output
+	GPIO.setup(TouchPin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+	GPIO.output(LedPin, GPIO.HIGH) # Set LedPin high(+3.3V) to off led
+
+def loop():
+	while True:
+		if GPIO.input(TouchPin) == GPIO.LOW:
+			print '...led on'
+			GPIO.output(LedPin, GPIO.LOW)  # led on
+		else:
+			print 'led off...'
+			GPIO.output(LedPin, GPIO.HIGH) # led off
+
+def destroy():
+	GPIO.output(LedPin, GPIO.HIGH)     # led off
+	GPIO.cleanup()                     # Release resource
+
+if __name__ == '__main__':     # Program start from here
+	setup()
+	try:
+		loop()
+	except KeyboardInterrupt:  # When 'Ctrl+C' is pressed, the child program destroy() will be  executed.
+		destroy()
+
