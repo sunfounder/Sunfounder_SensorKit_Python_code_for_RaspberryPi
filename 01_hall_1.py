c7ca529ab479e5c913243263bfabaead00a7a8ac
lnitram <martinleuschner@googlemail.com>
2015-06-22 23:47:05 +0200

    Use interrupt to detect falling edge This improves performance and prints only messages if event was detected LED part is still missing Can be improved by listening for BOTH events

--- 01_hall_1.py
+++ 01_hall_1.py
@@ -1,32 +1,24 @@
 #!/usr/bin/env python
 import RPi.GPIO as GPIO
 
-HallPin = 11    # pin11 --- hall
-LedPin  = 12    # pin12 --- led
+HALL = 11
 
-def setup():
-	GPIO.setmode(GPIO.BOARD)       # Numbers GPIOs by physical location
-	GPIO.setup(LedPin, GPIO.OUT)   # Set LedPin's mode is output
-	GPIO.setup(HallPin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-	GPIO.output(LedPin, GPIO.HIGH) # Set LedPin high(+3.3V) to off led
+GPIO.setmode(GPIO.BOARD)
+GPIO.setwarnings(False)
+GPIO.setup(HALL,GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
-def loop():
-	while True:
-		if GPIO.input(HallPin) == GPIO.LOW:
-			print '...led on'
-			GPIO.output(LedPin, GPIO.LOW)  # led on
-		else:
-			print 'led off...'
-			GPIO.output(LedPin, GPIO.HIGH) # led off
+def magnet_detected(channel):
+        print "Magnetic material detected"  
 
-def destroy():
-	GPIO.output(LedPin, GPIO.HIGH)     # led off
-	GPIO.cleanup()                     # Release resource
+GPIO.add_event_detect(HALL, GPIO.FALLING, callback=magnet_detected)
 
-if __name__ == '__main__':     # Program start from here
-	setup()
-	try:
-		loop()
-	except KeyboardInterrupt:  # When 'Ctrl+C' is pressed, the child program destroy() will be  executed.
-		destroy()
+try:
+    while True:
+        pass
+
+except KeyboardInterrupt:
+    print "Ctrl-C - quit"
+
+finally:
+    GPIO.cleanup() 
 
