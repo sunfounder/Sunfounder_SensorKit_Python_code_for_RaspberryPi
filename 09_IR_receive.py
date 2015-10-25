fc7ac27869c22b0837009d59e650725fd707bf5e
lnitram <martinleuschner@googlemail.com>
2015-10-25 12:12:29 +0000

    Fix mixed LOW and HIGH usage

--- 09_IR_receive.py
+++ 09_IR_receive.py
@@ -10,16 +10,16 @@ def setup():
 	GPIO.setmode(GPIO.BOARD)       # Numbers GPIOs by physical location
 	GPIO.setup(LedPin, GPIO.OUT)   # Set LedPin's mode is output
 	GPIO.setup(IrPin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-	GPIO.output(LedPin, GPIO.HIGH) # Set LedPin high(+3.3V) to off led
+	GPIO.output(LedPin, GPIO.LOW) # Set LedPin high(+3.3V) to off led
 
 def swLed(ev=None):
 	global Led_status
 	Led_status = not Led_status
 	GPIO.output(LedPin, Led_status)  # switch led status(on-->off; off-->on)
 	if Led_status == 1:
-		print 'led off...'
+		print 'led on...'
 	else:
-		print '...led on'
+		print '...led off'
 
 def loop():
 	GPIO.add_event_detect(IrPin, GPIO.FALLING, callback=swLed) # wait for falling
@@ -27,7 +27,7 @@ def loop():
 		pass   # Don't do anything
 
 def destroy():
-	GPIO.output(LedPin, GPIO.HIGH)     # led off
+	GPIO.output(LedPin, GPIO.LOW)     # led off
 	GPIO.cleanup()                     # Release resource
 
 if __name__ == '__main__':     # Program start from here
