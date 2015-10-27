6ed1b3bb4e8d0d940c1ee4c7532de9103bf22bed
lnitram <martinleuschner@googlemail.com>
2015-10-27 21:21:47 +0000

    Fix mixed LOW and HIGH usage

--- 11_buzzer.py
+++ 11_buzzer.py
@@ -7,17 +7,17 @@ BuzzerPin = 11    # pin11
 def setup():
 	GPIO.setmode(GPIO.BOARD)       # Numbers GPIOs by physical location
 	GPIO.setup(BuzzerPin, GPIO.OUT)
-	GPIO.output(BuzzerPin, GPIO.HIGH)
+	GPIO.output(BuzzerPin, GPIO.LOW)
 
 def loop():
 	while True:
-		GPIO.output(BuzzerPin, GPIO.LOW)
-		time.sleep(0.5)
 		GPIO.output(BuzzerPin, GPIO.HIGH)
+		time.sleep(0.5)
+		GPIO.output(BuzzerPin, GPIO.LOW)
 		time.sleep(0.5)
 
 def destroy():
-	GPIO.output(BuzzerPin, GPIO.HIGH)
+	GPIO.output(BuzzerPin, GPIO.LOW)
 	GPIO.cleanup()                     # Release resource
 
 if __name__ == '__main__':     # Program start from here
