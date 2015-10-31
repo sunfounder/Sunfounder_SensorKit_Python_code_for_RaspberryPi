539bf8a4566c5a00caab85922b1d67b3fc44a449
Kaarel Kaljurand <kaljurand@gmail.com>
2015-10-31 22:03:53 +0100

    Fix typo (in code): destory() -> destroy()

--- ADC0832.py
+++ ADC0832.py
@@ -100,5 +100,5 @@ if __name__ == '__main__':		# Program start from here
 	try:
 		loop()
 	except KeyboardInterrupt:  	# When 'Ctrl+C' is pressed, the child program destroy() will be  executed.
-		destory()
+		destroy()
 
