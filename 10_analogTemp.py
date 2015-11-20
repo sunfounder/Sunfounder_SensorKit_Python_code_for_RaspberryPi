9e1db8b289524d197c0928e1046680183c97893e
Cavon Lee <lijiarong@sunfounder.com>
2015-11-20 18:32:40 +0800

    add temperature calaulation

--- 10_analogTemp.py
+++ 10_analogTemp.py
@@ -1,14 +1,19 @@
 #!/usr/bin/env python
 import ADC0832
 import time
+import math
 
 def init():
 	ADC0832.setup()
 
 def loop():
 	while True:
-		res = ADC0832.getResult()
-		print 'res = %d' % res
+		analogVal = ADC0832.getResult()
+		Vr = 5 * float(analogVal) / 255
+		Rt = 10000 * Vr / (5 - Vr)
+		temp = 1/(((math.log(Rt / 10000)) / 3950) + (1 / (273.15+25)))
+		temp = temp - 273.15
+		print 'temperature = %d C' % temp
 		time.sleep(0.2)
 
 if __name__ == '__main__':
